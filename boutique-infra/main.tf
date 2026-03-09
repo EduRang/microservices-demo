0a351eac0f00263fc1e8a02f8fa79b84580e6541
EduRang <edrango11@gmail.com>
2026-03-08 23:11:35 -0600

    feat: setup CI/CD pipeline

---
+++ boutique-infra/main.tf
@@ -0,0 +1,35 @@
+# Provider (Google Cloud)
+provider "google" {
+  project = "project-508b67ba-bc4a-4c1c-9dc"
+  region  = "us-central1"
+}
+
+# Necessary APIs
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+# GKE Autopilot Cluster
+resource "google_container_cluster" "primary" {
+  name     = "online-boutique-tf"
+  location = "us-central1"
+
+  enable_autopilot = true
+
+  depends_on = [google_project_service.container]
+}
+
+# Artifact Registry API
+resource "google_project_service" "artifactregistry" {
+  service = "artifactregistry.googleapis.com"
+}
+
+# Docker Repository
+resource "google_artifact_registry_repository" "boutique_repo" {
+  location      = "us-central1"
+  repository_id = "boutique-repo"
+  description   = "Docker repository for Online Boutique images"
+  format        = "DOCKER"
+
+  depends_on = [google_project_service.artifactregistry]
+}
